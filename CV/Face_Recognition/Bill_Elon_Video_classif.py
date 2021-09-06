52877aab7f48b683fb241c0c5144a1c299c43cb2
Alik Mondal <89861119+AlikMon88@users.noreply.github.com>
2021-09-07 00:23:21 +0530

    Update Bill_Elon_Video_classif.py

--- CV/Face_Recognition/Bill_Elon_Video_classif.py
+++ CV/Face_Recognition/Bill_Elon_Video_classif.py
@@ -1,3 +1,43 @@
+## Training of Model
+import os 
+import cv2 as cv
+import numpy as np
+
+path=r'F:\Train_face'
+people = os.listdir(path)
+feature=[]
+label=[]
+haar_cascade=cv.CascadeClassifier('har_cascade.xml')
+def create_train():    
+    for cb in people:
+        cb_nxt = os.path.join(path,cb)
+        labels=people.index(cb)
+        for img in os.listdir(cb_nxt):
+            img_path=os.path.join(cb_nxt,img)
+            img=cv.imread(img_path)
+            gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+            faces_rect=haar_cascade.detectMultiScale(gray,1.1,4)
+
+            for (x,y,w,h) in faces_rect:
+                img_data=gray[y:y+h,x:x+w]
+                feature.append(img_data)
+                label.append(labels)
+create_train()
+feature=np.array(feature)
+label=np.array(label)
+recgn = cv.face.LBPHFaceRecognizer_create()
+recgn.train(feature,label)
+
+recgn.save('face_recog.yml')
+
+print('Training Done!!........')
+
+
+np.save('label.npy',label)
+np.save('feature.npy',feature)
+
+
 #Testing_on_Videos
 import os
 path= r'F:\Train_face'
